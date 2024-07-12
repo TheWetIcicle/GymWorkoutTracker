10d72f6f367881c680bd0326b8df8a1a8fd3968b
Aditya <adipandey901@gmail.com>
2024-07-12 10:18:44 +0400

    Finishing up the frontend and doing some small changes

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
+
+//Components
+import WorkoutDetails from '../components/workoutDetails'
+import WorkoutForm from '../components/workoutForm'
+
 const Home = () => {
+    const {workouts, dispatch} = useWorkoutContext()
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workoutRoutes')
+            const json = await response.json()
+
+            if (response.ok) {
+                dispatch({type: 'SET_WORKOUTS', payload: json})
+            }
+        }
+
+        fetchWorkouts()
+    }, [dispatch])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
+            <WorkoutForm />
         </div>
     )
 }
